20bb32278117ba7481d6cd5cbd31fd42771fa906
Barinade <poco0317@users.noreply.github.com>
2023-06-03 16:58:57 -0500

    exceptoin handel

--- src/main/java/com/etterna/services/MultiplayerRequestService.java
+++ src/main/java/com/etterna/services/MultiplayerRequestService.java
@@ -1,5 +1,6 @@
 package com.etterna.services;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -31,56 +32,66 @@ public class MultiplayerRequestService {
 	
 	public List<PlayerDTO> getOnlinePlayers() {
 		m_logger.info("Sending request to get all online players");
-		RestTemplate restCall = new RestTemplate();
-		ResponseEntity<List<PlayerDTO>> response = restCall.exchange(api(ONLINE_PLAYERS), HttpMethod.GET, null, new ParameterizedTypeReference<List<PlayerDTO>>() {});
-		
-		List<PlayerDTO> o = response.getBody();
-		o.sort(new Comparator<PlayerDTO>() {
-			@Override
-			public int compare(PlayerDTO o1, PlayerDTO o2) {
-				String l1 = o1.getLobby();
-				String l2 = o2.getLobby();
-				if (l1 == null) {
-					return 1;
+		try {
+			RestTemplate restCall = new RestTemplate();
+			ResponseEntity<List<PlayerDTO>> response = restCall.exchange(api(ONLINE_PLAYERS), HttpMethod.GET, null, new ParameterizedTypeReference<List<PlayerDTO>>() {});
+			
+			List<PlayerDTO> o = response.getBody();
+			o.sort(new Comparator<PlayerDTO>() {
+				@Override
+				public int compare(PlayerDTO o1, PlayerDTO o2) {
+					String l1 = o1.getLobby();
+					String l2 = o2.getLobby();
+					if (l1 == null) {
+						return 1;
+					}
+					int o = l1.compareToIgnoreCase(l2);
+					if (o == 0) {
+						l1 = o1.getName();
+						l2 = o2.getName();
+						o = l1.compareToIgnoreCase(l2);
+					}
+					return o;
 				}
-				int o = l1.compareToIgnoreCase(l2);
-				if (o == 0) {
-					l1 = o1.getName();
-					l2 = o2.getName();
-					o = l1.compareToIgnoreCase(l2);
-				}
-				return o;
-			}
-		});
-		m_logger.info("Got back {} players", o.size());
-		return o;
+			});
+			m_logger.info("Got back {} players", o.size());
+			return o;
+		} catch (Exception e) {
+			m_logger.error(e.getMessage(), e);
+			return new ArrayList<>();
+		}
 	}
 	
 	public List<PlayerDTO> getPlayersInLobby(String lobbyName) {
 		m_logger.info("Sending request to get players in lobby '{}'", lobbyName);
-		RestTemplate restCall = new RestTemplate();
-		ResponseEntity<List<PlayerDTO>> response = restCall.exchange(api(PLAYERS_IN_LOBBY) + lobbyName.toLowerCase(), HttpMethod.GET, null, new ParameterizedTypeReference<List<PlayerDTO>>() {});
-		
-		List<PlayerDTO> o = response.getBody();
-		o.sort(new Comparator<PlayerDTO>() {
-			@Override
-			public int compare(PlayerDTO o1, PlayerDTO o2) {
-				String l1 = o1.getLobby();
-				String l2 = o2.getLobby();
-				if (l1 == null) {
-					return 1;
+		try {
+			RestTemplate restCall = new RestTemplate();
+			ResponseEntity<List<PlayerDTO>> response = restCall.exchange(api(PLAYERS_IN_LOBBY) + lobbyName.toLowerCase(), HttpMethod.GET, null, new ParameterizedTypeReference<List<PlayerDTO>>() {});
+			
+			List<PlayerDTO> o = response.getBody();
+			o.sort(new Comparator<PlayerDTO>() {
+				@Override
+				public int compare(PlayerDTO o1, PlayerDTO o2) {
+					String l1 = o1.getLobby();
+					String l2 = o2.getLobby();
+					if (l1 == null) {
+						return 1;
+					}
+					int o = l1.compareToIgnoreCase(l2);
+					if (o == 0) {
+						l1 = o1.getName();
+						l2 = o2.getName();
+						o = l1.compareToIgnoreCase(l2);
+					}
+					return o;
 				}
-				int o = l1.compareToIgnoreCase(l2);
-				if (o == 0) {
-					l1 = o1.getName();
-					l2 = o2.getName();
-					o = l1.compareToIgnoreCase(l2);
-				}
-				return o;
-			}
-		});
-		m_logger.info("Got back {} players", o.size());
-		return o;		
+			});
+			m_logger.info("Got back {} players", o.size());
+			return o;
+		} catch (Exception e) {
+			m_logger.error(e.getMessage(), e);
+			return new ArrayList<>();
+		}
 	}
 
 }
